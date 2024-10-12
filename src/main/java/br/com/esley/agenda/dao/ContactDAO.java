3dae114c6933de21877e59c1fe7958503b04e519
EsleyFerreira <esley.fs25@gmail.com>
2024-10-12 05:56:08 -0300

    feat: adding ContactDao update

--- src/main/java/br/com/esley/agenda/dao/ContactDAO.java
+++ src/main/java/br/com/esley/agenda/dao/ContactDAO.java
@@ -6,6 +6,9 @@ import br.com.esley.agenda.repository.MySqlConnection;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ContactDAO {
     public void save(Contacts contacts) {
@@ -16,11 +19,13 @@ public class ContactDAO {
         try {
             conn = MySqlConnection.createConnectionToMySQL();
             pstm = (PreparedStatement) conn.prepareStatement(sql);
+
             pstm.setString(1, contacts.getName());
             pstm.setInt(2, contacts.getAge());
             pstm.setDate(3, new Date(contacts.getDataRegister().getTime()));
 
             pstm.execute();
+
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
@@ -35,5 +40,52 @@ public class ContactDAO {
                 e.printStackTrace();
             }
         }
+    }
+
+    public List<Contacts> getContacts() {
+        String sql = "SELECT * FROM contacts";
+
+        List<Contacts> contacts = new ArrayList<Contacts>();
+
+        Connection conn = null;
+        PreparedStatement pstm = null;
+        ResultSet rset = null;
+
+        try {
+            conn = MySqlConnection.createConnectionToMySQL();
+            pstm = conn.prepareStatement(sql);
+            rset = pstm.executeQuery();
+
+            while (rset.next()) {
+
+                Contacts contact = new Contacts();
+
+                contact.setId(rset.getInt("id"));
+                contact.setName(rset.getString("name"));
+                contact.setAge(rset.getInt("age"));
+                contact.setDataRegister(rset.getDate("registration_date"));
+
+                contacts.add(contact);
+            }
+            System.out.println("Contato Salvo!");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (rset != null) {
+                    rset.close();
+                }
+                if (pstm != null){
+                    pstm.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return contacts;
     }
 }
